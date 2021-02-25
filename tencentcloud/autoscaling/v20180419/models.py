b3f0f2953b1d827c6a7b16cf07150443db33380d
tencentcloudapi <tencentcloudapi@tenent.com>
2021-02-25 09:11:53 +0800

    release 3.0.170

--- tencentcloud/autoscaling/v20180419/models.py
+++ tencentcloud/autoscaling/v20180419/models.py
@@ -405,9 +405,9 @@ class CreateAutoScalingGroupFromInstanceRequest(AbstractModel):
         :type AutoScalingGroupName: str
         :param InstanceId: The instance ID.
         :type InstanceId: str
-        :param MinSize: The maximum number of instances. Value range: 0 - 2000.
+        :param MinSize: The minimum number of instances. Value range: 0-2000.
         :type MinSize: int
-        :param MaxSize: The minimum number of instances. Value range: 0 - 2000.
+        :param MaxSize: The maximum number of instances. Value range: 0-2000.
         :type MaxSize: int
         :param DesiredCapacity: The desired capacity. Its value must be greater than the minimum and smaller than the maximum.
         :type DesiredCapacity: int
@@ -622,9 +622,9 @@ class CreateLaunchConfigurationRequest(AbstractModel):
         :type EnhancedService: :class:`tencentcloud.autoscaling.v20180419.models.EnhancedService`
         :param UserData: Base64-encoded custom data of up to 16 KB.
         :type UserData: str
-        :param InstanceChargeType: Instance billing type. CVM instances are POSTPAID_BY_HOUR by default.
-<br><li>POSTPAID_BY_HOUR: Pay-as-you-go on an hourly basis
-<br><li>SPOTPAID: Bidding
+        :param InstanceChargeType: Instance billing mode. CVM instances take `POSTPAID_BY_HOUR` by default. Valid values:
+<br><li>POSTPAID_BY_HOUR: pay-as-you-go hourly
+<br><li>SPOTPAID: spot instance
         :type InstanceChargeType: str
         :param InstanceMarketOptions: Market-related options of the instance, such as the parameters related to stop instances. If the billing method of instance is specified as bidding, this parameter must be passed in.
         :type InstanceMarketOptions: :class:`tencentcloud.autoscaling.v20180419.models.InstanceMarketOptionsRequest`
@@ -758,7 +758,7 @@ class CreateLifecycleHookRequest(AbstractModel):
         :type DefaultResult: str
         :param HeartbeatTimeout: The maximum length of time (in seconds) that can elapse before the lifecycle hook times out. Value range: 30-3,600. Default value: 300
         :type HeartbeatTimeout: int
-        :param NotificationMetadata: Additional information sent by Auto Scaling to the notification target. Default value is ''. Maximum length is 1024 characters.
+        :param NotificationMetadata: Additional information of a notification that Auto Scaling sends to targets. This parameter is left empty by default. Up to 1024 characters are allowed.
         :type NotificationMetadata: str
         :param NotificationTarget: Notification target
         :type NotificationTarget: :class:`tencentcloud.autoscaling.v20180419.models.NotificationTarget`
@@ -2977,8 +2977,8 @@ class ModifyLaunchConfigurationAttributesRequest(AbstractModel):
         :type LaunchConfigurationId: str
         :param ImageId: Valid [image](https://intl.cloud.tencent.com/document/product/213/4940?from_cn_redirect=1) ID in the format of `img-8toqc6s3`. There are four types of images: <br/><li>Public images </li><li>Custom images </li><li>Shared images </li><li>Marketplace images </li><br/>You can obtain the available image IDs in the following ways: <br/><li>For `public images`, `custom images`, and `shared images`, log in to the [console](https://console.cloud.tencent.com/cvm/image?rid=1&imageType=PUBLIC_IMAGE) to query the image IDs; for `marketplace images`, query the image IDs through [Cloud Marketplace](https://market.cloud.tencent.com/list). </li><li>This value can be obtained from the `ImageId` field in the return value of the [DescribeImages API](https://intl.cloud.tencent.com/document/api/213/15715?from_cn_redirect=1).</li>
         :type ImageId: str
-        :param InstanceTypes: List of instance types. Different instance models specify different resource specifications. Up to 5 instance models can be supported.
-The launch configuration uses InstanceType to indicate one single instance type and InstanceTypes to indicate multiple instance types. After InstanceTypes is successfully specified for the launch configuration, the original InstanceType will be automatically invalidated.
+        :param InstanceTypes: List of instance types. Each type specifies different resource specifications. This list contains up to 10 instance types.
+The launch configuration uses `InstanceType` to indicate one single instance type and `InstanceTypes` to indicate multiple instance types. After `InstanceTypes` is successfully specified for the launch configuration, the original `InstanceType` will be automatically invalidated.
         :type InstanceTypes: list of str
         :param InstanceTypesCheckPolicy: Instance type verification policy which works when InstanceTypes is actually modified. Value range: ALL, ANY. Default value: ANY.
 <br><li> ALL: The verification will success only if all instance types (InstanceType) are available; otherwise, an error will be reported.
@@ -2991,6 +2991,22 @@ If a model in InstanceTypes does not exist or has been discontinued, a verificat
         :type LaunchConfigurationName: str
         :param UserData: Base64-encoded custom data of up to 16 KB. If you want to clear UserData, specify it as an empty string
         :type UserData: str
+        :param SecurityGroupIds: Security group to which the instance belongs. This parameter can be obtained from the `SecurityGroupId` field in the response of the [`DescribeSecurityGroups`](https://intl.cloud.tencent.com/document/api/215/15808?from_cn_redirect=1) API.
+At least one security group is required for this parameter. The security group specified is sequential.
+        :type SecurityGroupIds: list of str
+        :param InternetAccessible: Information of the public network bandwidth configuration.
+To modify it or even its subfield, you should specify all the subfields again.
+        :type InternetAccessible: :class:`tencentcloud.autoscaling.v20180419.models.InternetAccessible`
+        :param InstanceChargeType: Instance billing mode. Valid values:
+<br><li>POSTPAID_BY_HOUR: pay-as-you-go hourly
+<br><li>SPOTPAID: spot instance
+        :type InstanceChargeType: str
+        :param InstanceChargePrepaid: 
+        :type InstanceChargePrepaid: :class:`tencentcloud.autoscaling.v20180419.models.InstanceChargePrepaid`
+        :param InstanceMarketOptions: Market-related options for instances, such as parameters related to spot instances.
+This parameter is required when changing the instance billing mode to spot instance. It will be automatically discarded after the spot instance is changed to another instance billing mode.
+To modify it or even its subfield, you should specify all the subfields again.
+        :type InstanceMarketOptions: :class:`tencentcloud.autoscaling.v20180419.models.InstanceMarketOptionsRequest`
         """
         self.LaunchConfigurationId = None
         self.ImageId = None
@@ -2998,6 +3014,11 @@ If a model in InstanceTypes does not exist or has been discontinued, a verificat
         self.InstanceTypesCheckPolicy = None
         self.LaunchConfigurationName = None
         self.UserData = None
+        self.SecurityGroupIds = None
+        self.InternetAccessible = None
+        self.InstanceChargeType = None
+        self.InstanceChargePrepaid = None
+        self.InstanceMarketOptions = None
 
 
     def _deserialize(self, params):
@@ -3007,6 +3028,17 @@ If a model in InstanceTypes does not exist or has been discontinued, a verificat
         self.InstanceTypesCheckPolicy = params.get("InstanceTypesCheckPolicy")
         self.LaunchConfigurationName = params.get("LaunchConfigurationName")
         self.UserData = params.get("UserData")
+        self.SecurityGroupIds = params.get("SecurityGroupIds")
+        if params.get("InternetAccessible") is not None:
+            self.InternetAccessible = InternetAccessible()
+            self.InternetAccessible._deserialize(params.get("InternetAccessible"))
+        self.InstanceChargeType = params.get("InstanceChargeType")
+        if params.get("InstanceChargePrepaid") is not None:
+            self.InstanceChargePrepaid = InstanceChargePrepaid()
+            self.InstanceChargePrepaid._deserialize(params.get("InstanceChargePrepaid"))
+        if params.get("InstanceMarketOptions") is not None:
+            self.InstanceMarketOptions = InstanceMarketOptionsRequest()
+            self.InstanceMarketOptions._deserialize(params.get("InstanceMarketOptions"))
 
 
 class ModifyLaunchConfigurationAttributesResponse(AbstractModel):
@@ -3790,9 +3822,9 @@ class UpgradeLaunchConfigurationRequest(AbstractModel):
         :type DataDisks: list of DataDisk
         :param EnhancedService: Enhanced service. This parameter is used to specify whether to enable Cloud Security, Cloud Monitoring and other services. If this parameter is not specified, Cloud Monitoring and Cloud Security will be enabled by default.
         :type EnhancedService: :class:`tencentcloud.autoscaling.v20180419.models.EnhancedService`
-        :param InstanceChargeType: Instance billing type. CVM instances are POSTPAID_BY_HOUR by default.
-<br><li>POSTPAID_BY_HOUR: Pay-as-you-go on an hourly basis
-<br><li>SPOTPAID: Bidding
+        :param InstanceChargeType: Instance billing mode. CVM instances take `POSTPAID_BY_HOUR` by default. Valid values:
+<br><li>POSTPAID_BY_HOUR: pay-as-you-go hourly
+<br><li>SPOTPAID: spot instance
         :type InstanceChargeType: str
         :param InstanceMarketOptions: Market-related options of the instance, such as the parameters related to stop instances. If the billing method of instance is specified as bidding, this parameter must be passed in.
         :type InstanceMarketOptions: :class:`tencentcloud.autoscaling.v20180419.models.InstanceMarketOptionsRequest`
@@ -3931,7 +3963,7 @@ class UpgradeLifecycleHookRequest(AbstractModel):
         :type DefaultResult: str
         :param HeartbeatTimeout: The maximum length of time (in seconds) that can elapse before the lifecycle hook times out. Value range: 30-3,600. Default value: 300
         :type HeartbeatTimeout: int
-        :param NotificationMetadata: Additional information sent by AS to the notification target. The default value is ''
+        :param NotificationMetadata: Additional information of a notification that Auto Scaling sends to targets. This parameter is left empty by default.
         :type NotificationMetadata: str
         :param NotificationTarget: Notification target
         :type NotificationTarget: :class:`tencentcloud.autoscaling.v20180419.models.NotificationTarget`
