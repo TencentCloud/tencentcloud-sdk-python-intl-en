d2ce6670824c5fcb84416bbac78004b8a8e7b9cb
tencentcloudapi <tencentcloudapi@tencent.com>
2022-06-23 07:13:25 +0800

    release 3.0.487

---
+++ tests/unit/test_import.py
@@ -0,0 +1,429 @@
+# -*- coding: utf8 -*-
+# Copyright (c) 2017-2021 THL A29 Limited, a Tencent company. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def test_import_ams():
+    from tencentcloud.ams.v20201229 import ams_client
+    from tencentcloud.ams.v20201229 import models
+
+
+def test_import_antiddos():
+    from tencentcloud.antiddos.v20200309 import antiddos_client
+    from tencentcloud.antiddos.v20200309 import models
+
+
+def test_import_apigateway():
+    from tencentcloud.apigateway.v20180808 import apigateway_client
+    from tencentcloud.apigateway.v20180808 import models
+
+
+def test_import_autoscaling():
+    from tencentcloud.autoscaling.v20180419 import autoscaling_client
+    from tencentcloud.autoscaling.v20180419 import models
+
+
+def test_import_batch():
+    from tencentcloud.batch.v20170312 import batch_client
+    from tencentcloud.batch.v20170312 import models
+
+
+def test_import_billing():
+    from tencentcloud.billing.v20180709 import billing_client
+    from tencentcloud.billing.v20180709 import models
+
+
+def test_import_cam():
+    from tencentcloud.cam.v20190116 import cam_client
+    from tencentcloud.cam.v20190116 import models
+
+
+def test_import_cbs():
+    from tencentcloud.cbs.v20170312 import cbs_client
+    from tencentcloud.cbs.v20170312 import models
+
+
+def test_import_cdb():
+    from tencentcloud.cdb.v20170320 import cdb_client
+    from tencentcloud.cdb.v20170320 import models
+
+
+def test_import_cdn():
+    from tencentcloud.cdn.v20180606 import cdn_client
+    from tencentcloud.cdn.v20180606 import models
+
+
+def test_import_cfs():
+    from tencentcloud.cfs.v20190719 import cfs_client
+    from tencentcloud.cfs.v20190719 import models
+
+
+def test_import_chdfs():
+    from tencentcloud.chdfs.v20201112 import chdfs_client
+    from tencentcloud.chdfs.v20201112 import models
+
+
+def test_import_ciam():
+    from tencentcloud.ciam.v20210420 import ciam_client
+    from tencentcloud.ciam.v20210420 import models
+
+
+def test_import_ckafka():
+    from tencentcloud.ckafka.v20190819 import ckafka_client
+    from tencentcloud.ckafka.v20190819 import models
+
+
+def test_import_clb():
+    from tencentcloud.clb.v20180317 import clb_client
+    from tencentcloud.clb.v20180317 import models
+
+
+def test_import_cloudaudit():
+    from tencentcloud.cloudaudit.v20190319 import cloudaudit_client
+    from tencentcloud.cloudaudit.v20190319 import models
+
+
+def test_import_cls():
+    from tencentcloud.cls.v20201016 import cls_client
+    from tencentcloud.cls.v20201016 import models
+
+
+def test_import_cmq():
+    from tencentcloud.cmq.v20190304 import cmq_client
+    from tencentcloud.cmq.v20190304 import models
+
+
+def test_import_cvm():
+    from tencentcloud.cvm.v20170312 import cvm_client
+    from tencentcloud.cvm.v20170312 import models
+
+
+def test_import_cynosdb():
+    from tencentcloud.cynosdb.v20190107 import cynosdb_client
+    from tencentcloud.cynosdb.v20190107 import models
+
+
+def test_import_dayu():
+    from tencentcloud.dayu.v20180709 import dayu_client
+    from tencentcloud.dayu.v20180709 import models
+
+
+def test_import_dbbrain():
+    from tencentcloud.dbbrain.v20191016 import dbbrain_client
+    from tencentcloud.dbbrain.v20191016 import models
+
+
+def test_import_dbbrain():
+    from tencentcloud.dbbrain.v20210527 import dbbrain_client
+    from tencentcloud.dbbrain.v20210527 import models
+
+
+def test_import_dc():
+    from tencentcloud.dc.v20180410 import dc_client
+    from tencentcloud.dc.v20180410 import models
+
+
+def test_import_dcdb():
+    from tencentcloud.dcdb.v20180411 import dcdb_client
+    from tencentcloud.dcdb.v20180411 import models
+
+
+def test_import_dms():
+    from tencentcloud.dms.v20200819 import dms_client
+    from tencentcloud.dms.v20200819 import models
+
+
+def test_import_dts():
+    from tencentcloud.dts.v20180330 import dts_client
+    from tencentcloud.dts.v20180330 import models
+
+
+def test_import_eb():
+    from tencentcloud.eb.v20210416 import eb_client
+    from tencentcloud.eb.v20210416 import models
+
+
+def test_import_ecdn():
+    from tencentcloud.ecdn.v20191012 import ecdn_client
+    from tencentcloud.ecdn.v20191012 import models
+
+
+def test_import_ecm():
+    from tencentcloud.ecm.v20190719 import ecm_client
+    from tencentcloud.ecm.v20190719 import models
+
+
+def test_import_eiam():
+    from tencentcloud.eiam.v20210420 import eiam_client
+    from tencentcloud.eiam.v20210420 import models
+
+
+def test_import_emr():
+    from tencentcloud.emr.v20190103 import emr_client
+    from tencentcloud.emr.v20190103 import models
+
+
+def test_import_es():
+    from tencentcloud.es.v20180416 import es_client
+    from tencentcloud.es.v20180416 import models
+
+
+def test_import_faceid():
+    from tencentcloud.faceid.v20180301 import faceid_client
+    from tencentcloud.faceid.v20180301 import models
+
+
+def test_import_gaap():
+    from tencentcloud.gaap.v20180529 import gaap_client
+    from tencentcloud.gaap.v20180529 import models
+
+
+def test_import_gme():
+    from tencentcloud.gme.v20180711 import gme_client
+    from tencentcloud.gme.v20180711 import models
+
+
+def test_import_gpm():
+    from tencentcloud.gpm.v20200820 import gpm_client
+    from tencentcloud.gpm.v20200820 import models
+
+
+def test_import_gse():
+    from tencentcloud.gse.v20191112 import gse_client
+    from tencentcloud.gse.v20191112 import models
+
+
+def test_import_iai():
+    from tencentcloud.iai.v20200303 import iai_client
+    from tencentcloud.iai.v20200303 import models
+
+
+def test_import_ims():
+    from tencentcloud.ims.v20201229 import ims_client
+    from tencentcloud.ims.v20201229 import models
+
+
+def test_import_iotcloud():
+    from tencentcloud.iotcloud.v20210408 import iotcloud_client
+    from tencentcloud.iotcloud.v20210408 import models
+
+
+def test_import_ip():
+    from tencentcloud.ip.v20210409 import ip_client
+    from tencentcloud.ip.v20210409 import models
+
+
+def test_import_kms():
+    from tencentcloud.kms.v20190118 import kms_client
+    from tencentcloud.kms.v20190118 import models
+
+
+def test_import_lighthouse():
+    from tencentcloud.lighthouse.v20200324 import lighthouse_client
+    from tencentcloud.lighthouse.v20200324 import models
+
+
+def test_import_live():
+    from tencentcloud.live.v20180801 import live_client
+    from tencentcloud.live.v20180801 import models
+
+
+def test_import_mariadb():
+    from tencentcloud.mariadb.v20170312 import mariadb_client
+    from tencentcloud.mariadb.v20170312 import models
+
+
+def test_import_mdc():
+    from tencentcloud.mdc.v20200828 import mdc_client
+    from tencentcloud.mdc.v20200828 import models
+
+
+def test_import_mdl():
+    from tencentcloud.mdl.v20200326 import mdl_client
+    from tencentcloud.mdl.v20200326 import models
+
+
+def test_import_mdp():
+    from tencentcloud.mdp.v20200527 import mdp_client
+    from tencentcloud.mdp.v20200527 import models
+
+
+def test_import_mongodb():
+    from tencentcloud.mongodb.v20190725 import mongodb_client
+    from tencentcloud.mongodb.v20190725 import models
+
+
+def test_import_monitor():
+    from tencentcloud.monitor.v20180724 import monitor_client
+    from tencentcloud.monitor.v20180724 import models
+
+
+def test_import_mps():
+    from tencentcloud.mps.v20190612 import mps_client
+    from tencentcloud.mps.v20190612 import models
+
+
+def test_import_msp():
+    from tencentcloud.msp.v20180319 import msp_client
+    from tencentcloud.msp.v20180319 import models
+
+
+def test_import_ocr():
+    from tencentcloud.ocr.v20181119 import ocr_client
+    from tencentcloud.ocr.v20181119 import models
+
+
+def test_import_organization():
+    from tencentcloud.organization.v20181225 import organization_client
+    from tencentcloud.organization.v20181225 import models
+
+
+def test_import_postgres():
+    from tencentcloud.postgres.v20170312 import postgres_client
+    from tencentcloud.postgres.v20170312 import models
+
+
+def test_import_privatedns():
+    from tencentcloud.privatedns.v20201028 import privatedns_client
+    from tencentcloud.privatedns.v20201028 import models
+
+
+def test_import_redis():
+    from tencentcloud.redis.v20180412 import redis_client
+    from tencentcloud.redis.v20180412 import models
+
+
+def test_import_rum():
+    from tencentcloud.rum.v20210622 import rum_client
+    from tencentcloud.rum.v20210622 import models
+
+
+def test_import_scf():
+    from tencentcloud.scf.v20180416 import scf_client
+    from tencentcloud.scf.v20180416 import models
+
+
+def test_import_ses():
+    from tencentcloud.ses.v20201002 import ses_client
+    from tencentcloud.ses.v20201002 import models
+
+
+def test_import_sms():
+    from tencentcloud.sms.v20190711 import sms_client
+    from tencentcloud.sms.v20190711 import models
+
+
+def test_import_sms():
+    from tencentcloud.sms.v20210111 import sms_client
+    from tencentcloud.sms.v20210111 import models
+
+
+def test_import_sqlserver():
+    from tencentcloud.sqlserver.v20180328 import sqlserver_client
+    from tencentcloud.sqlserver.v20180328 import models
+
+
+def test_import_ssl():
+    from tencentcloud.ssl.v20191205 import ssl_client
+    from tencentcloud.ssl.v20191205 import models
+
+
+def test_import_ssm():
+    from tencentcloud.ssm.v20190923 import ssm_client
+    from tencentcloud.ssm.v20190923 import models
+
+
+def test_import_sts():
+    from tencentcloud.sts.v20180813 import sts_client
+    from tencentcloud.sts.v20180813 import models
+
+
+def test_import_tag():
+    from tencentcloud.tag.v20180813 import tag_client
+    from tencentcloud.tag.v20180813 import models
+
+
+def test_import_tat():
+    from tencentcloud.tat.v20201028 import tat_client
+    from tencentcloud.tat.v20201028 import models
+
+
+def test_import_tcaplusdb():
+    from tencentcloud.tcaplusdb.v20190823 import tcaplusdb_client
+    from tencentcloud.tcaplusdb.v20190823 import models
+
+
+def test_import_tcr():
+    from tencentcloud.tcr.v20190924 import tcr_client
+    from tencentcloud.tcr.v20190924 import models
+
+
+def test_import_tdmq():
+    from tencentcloud.tdmq.v20200217 import tdmq_client
+    from tencentcloud.tdmq.v20200217 import models
+
+
+def test_import_tem():
+    from tencentcloud.tem.v20201221 import tem_client
+    from tencentcloud.tem.v20201221 import models
+
+
+def test_import_tem():
+    from tencentcloud.tem.v20210701 import tem_client
+    from tencentcloud.tem.v20210701 import models
+
+
+def test_import_teo():
+    from tencentcloud.teo.v20220106 import teo_client
+    from tencentcloud.teo.v20220106 import models
+
+
+def test_import_tiw():
+    from tencentcloud.tiw.v20190919 import tiw_client
+    from tencentcloud.tiw.v20190919 import models
+
+
+def test_import_tke():
+    from tencentcloud.tke.v20180525 import tke_client
+    from tencentcloud.tke.v20180525 import models
+
+
+def test_import_tms():
+    from tencentcloud.tms.v20201229 import tms_client
+    from tencentcloud.tms.v20201229 import models
+
+
+def test_import_trtc():
+    from tencentcloud.trtc.v20190722 import trtc_client
+    from tencentcloud.trtc.v20190722 import models
+
+
+def test_import_vm():
+    from tencentcloud.vm.v20201229 import vm_client
+    from tencentcloud.vm.v20201229 import models
+
+
+def test_import_vod():
+    from tencentcloud.vod.v20180717 import vod_client
+    from tencentcloud.vod.v20180717 import models
+
+
+def test_import_vpc():
+    from tencentcloud.vpc.v20170312 import vpc_client
+    from tencentcloud.vpc.v20170312 import models
+
+
+def test_import_yunjing():
+    from tencentcloud.yunjing.v20180228 import yunjing_client
+    from tencentcloud.yunjing.v20180228 import models
