c674a8fd8e7ba668251611d7362bf3baf2d83cba
tencentcloudapi <tencentcloudapi@tencent.com>
2023-05-31 10:40:14 +0800

    release 3.0.716

--- tencentcloud/sms/v20210111/models.py
+++ tencentcloud/sms/v20210111/models.py
@@ -1482,7 +1482,7 @@ class SendSmsRequest(AbstractModel):
     def __init__(self):
         r"""
         :param PhoneNumberSet: Target mobile number in the E.164 standard in the format of +[country/region code][mobile number]. Up to 200 mobile numbers are supported in one request (which should be all Chinese mainland mobile numbers or all global mobile numbers).
-For example, +8613711112222, which has a + sign followed by 86 (country/region code) and then by 13711112222 (mobile number).
+For example, +60198890000, which has a + sign followed by 60 (country/region code) and then by 198890000 (mobile number).
         :type PhoneNumberSet: list of str
         :param SmsSdkAppId: The SMS `SdkAppId` generated after an application is added in the [SMS console](https://console.cloud.tencent.com/smsv2/app-manage), such as 1400006666.
         :type SmsSdkAppId: str
