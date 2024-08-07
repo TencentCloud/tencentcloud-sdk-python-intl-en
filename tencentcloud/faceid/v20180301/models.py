e245bef6ddf77e360f94b457904ad09087bd6d7a
tencentcloudapi <tencentcloudapi@tencent.com>
2024-08-08 05:05:41 +0800

    release 3.0.1018

--- tencentcloud/faceid/v20180301/models.py
+++ tencentcloud/faceid/v20180301/models.py
@@ -181,14 +181,14 @@ PASSPORT
 DRIVING_LICENSE
 AUTO
         :type CardType: str
-        :param _ImageBase64Front: Base64 value for the front of the document. Supported image formats: PNG, JPEG, GIF format is not supported yet. Supported image size: The downloaded image cannot exceed 5M after Base64 encoding. 
+        :param _ImageBase64Front: Base64 value for the front of the document. Supported image formats: PNG, JPEG. GIF format is not supported yet. Supported image size: The downloaded image cannot exceed 5M after Base64 encoding.
 The image download takes no more than 3 seconds. Supported image resolution: 8000*8000. One of ImageUrlFront and ImageBase64 Front of the image must be provided. If both are provided, only ImageUrlFront will be used.
         :type ImageBase64Front: str
-        :param _ImageBase64Back: Base64 value of the reverse side of the document. Supported image formats: PNG, JPEG, GIF format is not supported yet. Supported image size: The downloaded image cannot exceed 5M after Base64 encoding. The image download takes no more than 3 seconds. Maximum supported image resolution: 8000*8000. For some certificates, one of ImageUrlBack and ImageBase64Back must be provided. If both are provided, only ImageUrlBack will be used.
+        :param _ImageBase64Back: Base64 value of the reverse side of the document. Supported image formats: PNG, JPEG. GIF format is not supported yet. Supported image size: The downloaded image cannot exceed 5M after Base64 encoding. The image download takes no more than 3 seconds. Maximum supported image resolution: 8000*8000. For some certificates, one of ImageUrlBack and ImageBase64Back must be provided. If both are provided, only ImageUrlBack will be used.
         :type ImageBase64Back: str
-        :param _ImageUrlFront: The URL value on the back of the certificate. Supported image formats: PNG, JPEG, GIF format is not supported yet. Supported image size: The downloaded image cannot exceed 5M after Base64 encoding. The image download takes no more than 3 seconds. Maximum supported image resolution: 8000*8000. One of ImageUrlFront and ImageBase64Front of the image must be provided. If both are provided, only ImageUrlFront will be used.
+        :param _ImageUrlFront: The URL value on the back of the certificate. Supported image formats: PNG, JPEG. GIF format is not supported yet. Supported image size: The downloaded image cannot exceed 5M after Base64 encoding. The image download takes no more than 3 seconds. Maximum supported image resolution: 8000*8000. One of ImageUrlFront and ImageBase64Front of the image must be provided. If both are provided, only ImageUrlFront will be used.
         :type ImageUrlFront: str
-        :param _ImageUrlBack: The URL value on the back of the certificate. Supported image formats: PNG, JPEG, GIF format is not supported yet. Supported image size: The downloaded image cannot exceed 5M after Base64 encoding. The image download takes no more than 3 seconds. Maximum supported image resolution: 8000*8000. For some certificates, one of ImageUrlBack and ImageBase64Back must be provided. If both are provided, only ImageUrlBack will be used.
+        :param _ImageUrlBack: The URL value on the back of the certificate. Supported image formats: PNG, JPEG. GIF format is not supported yet. Supported image size: The downloaded image cannot exceed 5M after Base64 encoding. The image download takes no more than 3 seconds. Maximum supported image resolution: 8000*8000. For some certificates, one of ImageUrlBack and ImageBase64Back must be provided. If both are provided, only ImageUrlBack will be used.
         :type ImageUrlBack: str
         """
         self._Nationality = None
@@ -1345,8 +1345,8 @@ The size of image data encoded by Base64 shall not exceed 3M, only jpg and png a
 Please use standard Base64 encoding (use = for padding). Refer to RFC4648 for encoding specifications. 
 Example values: "/9j/4AAQSk... (total length:61944)KiiK//2Q=="
         :type ImageBase64: str
-        :param _VideoBase64: Base64 value of photos used for face comparison. 
-The size of image data encoded by Base64 shall not exceed 3M, only jpg and png are supported. 
+        :param _VideoBase64: Base64 value of videos used for face comparison. 
+The size of videos data encoded by Base64 shall not exceed 8M, only mp4,avi,flv are supported. 
 Please use standard Base64 encoding (use = for padding). Refer to RFC4648 for encoding specifications. 
 Example values: "/9j/4AAQSk... (total length:61944)KiiK//2Q=="
         :type VideoBase64: str
@@ -1354,7 +1354,7 @@ Example values: "/9j/4AAQSk... (total length:61944)KiiK//2Q=="
 `LIP`: Numeric mode; `ACTION`: Motion mode; `SILENT`: silent mode. Select one of them.
 Example value: "SILENT"
         :type LivenessType: str
-        :param _ValidateData: When the “LivenessType” parameter is “ACTION”, it must be specified.
+        :param _ValidateData: When the "LivenessType" parameter is "ACTION", it must be specified.
 It is used to control the action sequence. Action types: 
 1 (open mouth)
 2 (blink)
@@ -1363,7 +1363,7 @@ It is used to control the action sequence. Action types:
 Select one or two from the four actions.
 Example of passing single action parameter: "1".
 Example of passing multiple action parameters: "4,2".
-When the “LivenessType” parameter value is “SILENT”, it shall be unspecified.
+When the "LivenessType" parameter value is "SILENT", it shall be unspecified.
 Example value: ""
         :type ValidateData: str
         """
@@ -1433,12 +1433,12 @@ Example Value: "Success".
         :param _Description: Description of business results. 
 Example value: "Success"
         :type Description: str
-        :param _Sim: This value is valid when the “Result” parameter is "Success" or "FailedOperation.CompareLowSimilarity." 
-This value indicates the similarity of face comparison. Value range: [0.00, 100.00]. The false pass rate for threshold 70 is 1 in 1,000, and the false pass rate for threshold 80 is 1 in 1,000. 
+        :param _Sim: This value is valid when the "Result" parameter is "Success" or "FailedOperation.CompareLowSimilarity." 
+This value indicates the similarity of face comparison. Value range: [0.00, 100.00]. The false pass rate for threshold 70 is 1 in 1,000, and the false pass rate for threshold 80 is 1 in 10,000. 
 Example value: 80.00
         :type Sim: float
         :param _BestFrameBase64: The optimal screenshot of the video after verification is the value encoded by BASE64, jpg format. 
-Note: This field may return “null”, indicating that no valid value can be obtained. 
+Note: This field may return "null", indicating that no valid value can be obtained. 
 Example values: "/9j/4AAQSk... (total length:142036)s97n//2Q=="
         :type BestFrameBase64: str
         :param _RequestId: The unique request ID, generated by the server, will be returned for every request (if the request fails to reach the server for other reasons, the request will not obtain a RequestId). RequestId is required for locating a problem.
